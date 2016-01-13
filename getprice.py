44ac1ec336727d998115cfaa3b7d1bdca579e298
Hao Duan <evanslight@live.com>
2016-01-14 00:54:07 +0800

    get simple history price

---
+++ getprice.py
@@ -0,0 +1,11 @@
+import steamy
+import requests
+
+steamap = steamy.SteamAPI('4D0E1F0AA3D33E94FF26C1909271BAB1')
+
+# this 570 means game name. 570 stands for Dota2
+steam = steamap.market('570')
+
+offer = steam.get_item_price_history('Rockshell Scout')
+
+print(offer)
